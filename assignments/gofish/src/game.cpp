e9a52276dc50405330c3f49caeb2291f5f8f323a
riceriley59 <64724036+riceriley59@users.noreply.github.com>
2022-11-06 11:15:35 -0800

    fixed some more formatting

--- assignments/gofish/src/game.cpp
+++ assignments/gofish/src/game.cpp
@@ -351,7 +351,7 @@ void Game::computers_turn(int rank, bool& go_again){
     }
 
     int guess = this->guess_rank(rank);
-    cout << "\nThe computer asked for " << this->map_output_rank(rank) << "s\n";
+    cout << "\nThe computer asked for " << this->map_output_rank(guess) << "s\n";
     this->handle_guess_cards_computer(guess, go_again);
     this->check_for_books_computer();
 }
