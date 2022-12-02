067faaf3dcc92a948a8fcd145862ddb4d1f8c660
riceriley59 <64724036+riceriley59@users.noreply.github.com>
2022-12-01 17:13:16 -0800

    push to server

--- assignments/linkedlist/linked_list/src/node.cpp
+++ assignments/linkedlist/linked_list/src/node.cpp
@@ -9,14 +9,3 @@ Node::Node(int x){
     this->val = x;
     this->next = nullptr;
 }
-
-Node::Node(Node* node){
-    this->val = 0;
-    this->next = node;
-}
-
-Node::Node(Node* node, int x){
-    this->val = x;
-    this->next = node;
-}
-
